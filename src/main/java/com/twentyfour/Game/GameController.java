804674321db08d3c5550d678303b4e7f105870fa
om-arya <om.arya0577@gmail.com>
2024-01-19 20:41:14 -0500

    add Java files

---
+++ src/main/java/com/twentyfour/Game/GameController.java
@@ -0,0 +1,34 @@
+package com.twentyfour.Game;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+
+@CrossOrigin(origins = "http://localhost:3000/play")
+@Controller
+public class GameController {
+    private DeckInitializer deckInitializer = new DeckInitializer();
+    private Card[] deck = deckInitializer.initializeDeck(true, true, true);
+
+    /**
+     * Fetch the next card; invoked upon client launch and after solving a card.
+     * @return A random card from the deck.
+     */
+    @GetMapping("/getNextCard")
+    public ResponseEntity<Card> getNextCard() {
+        Card card = deck[(int) (Math.random() * deck.length)];
+        return ResponseEntity.ok(card);
+    }
+
+    /**
+     * Create a new deck to fetch cards from; invoked upon difficulty changes.
+     * @param hasEasy Include in the deck?
+     * @param hasMedium Include in the deck?
+     * @param hasHard Include in the deck?
+     */
+    @PostMapping("/createNewDeck")
+    @ResponseStatus(HttpStatus.OK)
+    public void createNewDeck(@RequestParam boolean hasEasy, @RequestParam boolean hasMedium,
+                              @RequestParam boolean hasHard) {
+        deck = deckInitializer.initializeDeck(hasEasy, hasMedium, hasHard);
+    }
+}
---
+++ src/main/java/com/twentyfour/Game/WebConfig.java
@@ -0,0 +1,22 @@
+package com.twentyfour.Game;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:3000/play");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
